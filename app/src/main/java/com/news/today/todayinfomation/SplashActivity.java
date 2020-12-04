3302660b4cadaa94ad2efabea16e01cd497a3976
zhaoyongzheng <3225651583@qq.com>
2020-12-04 09:55:57 +0800

    Initial commit

---
+++ app/src/main/java/com/news/today/todayinfomation/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.news.today.todayinfomation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 2020/12/4.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
